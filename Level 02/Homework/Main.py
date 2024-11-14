810a8832b90a88597c49562bdb4f56a6ab4fac11
Levan500Skhulukhia <big88lebovski@gmail.com>
2024-11-14 23:23:40 +0400

    commit

---
+++ Level 02/Homework/Main.py
@@ -0,0 +1,28 @@
+#Names 
+print("Irma Abshilava")
+print("Bejan Skhulukhia")
+print("Nika Keshelava")
+print("Giorgi Patashuri")
+print("Luka Mamulashvili")
+print("John Cena")
+print("Xi Jiping")
+print("Anold Schwarzenegger")
+
+#Mathematical Things
+print(5+8)
+print(5+17)
+print(5-23)
+print(4*8)
+print(9/3)
+a=25
+b=40
+print(a+b)
+c=1000
+d=4
+print(c/d)
+g=8
+f=("5")
+print(g*int(f))
+print(888+999)
+print(737+82)
+print(1000-7)
