1f33c53597c58e3369c4357b9b4424e769d7b425
Levan500Skhulukhia <big88lebovski@gmail.com>
2024-11-15 14:58:45 +0400

    commit

---
+++ Level 03/Homework/Main.py
@@ -0,0 +1,41 @@
+Name = ("Goga") # დავარქვათ ცვლადს მისი მნისშვნელობის სახელი, დავაწეროთ"=" და დავაწეროთ თვითონ რა იმალება მნიშვნელობის სახელის ქვეშ
+
+
+Num1=4
+Num2=15
+Num3=11
+print(Num1+Num2+Num3)
+
+X=7
+y=9
+print(X*y)
+
+length=20
+widht=10
+print(length + length + widht+ widht) # perimeter is 60sm
+
+
+age1=16
+age2=58
+age3=58
+print(age1+age2+age3)
+Result=(132)
+print(Result/3) #საშუალო არითმეტოკულია = 44.0
+
+
+firstname=("Levan")
+lastname=("Skhulukhia")
+Result2=(firstname+" "+lastname)
+print(Result2)
+
+fullname=("levan  skhulukhia  ")
+age=(16)
+print(fullname*age)
+
+#დეკლარაცია არის პირველი სიტყვა(ანუ ცვლადი) რომელიც =(ტოლიას)უკან დგას, ანუ ყუთის სახელი.
+#ინიციალიზება კი არის ის მნიშვნელობა რომელიც მაგ დეკლარაციასში(ყუთში)იმალება ეგ როგორც წესი იწერება =(ტოლიას)შემდეგ
+# რე-ინიციალიზება ანუ მაგ მნიშვნელობის ცვალება და ახალი მნიშვნელობის მინიჭება(ანუ ყუთიდან ძველი ნივთის ამოღება და ახლის ჩადება)
+Lika=("kai gogoa")
+
+Lika=("cudi gogoa")
+print(Lika)
